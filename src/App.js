4137d517be11f4b41f120afdb54c368b26129072
marcelagiraldo <leidymarcelaalzategiraldo@gmail.com>
2023-03-22 09:32:30 -0500

    first commit
    first change in App.js
    second change in App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Rescates UAM, Bienvenidos</h1>
+    <h2>Esperamos les sea de mucha utilidad nuestro sistema</h2>
+    <h3>Pronto habrán nuevos cambios</h3>
+    </div>
+  );
+}
+
+export default App;
